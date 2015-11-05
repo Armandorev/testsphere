18d05ffbf624a0c09f90885777f1c6ad9469ba5a
Armando Sánchez Medina <armandosanchezmedina@gmail.com>
2015-11-05 23:09:14 +0000

    Added First stupid test

--- src/test/java/BasicTests.java
+++ src/test/java/BasicTests.java
@@ -1,5 +1,14 @@
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+
 /**
  * Created by armandosanchezmedina on 05/11/2015.
  */
 public class BasicTests {
+    @Test
+    public void shouldGeneratePersonWithPersonalInformation(){
+        assertThat(new Person(), CoreMatchers.instanceOf(Person.class));
+    }
 }
