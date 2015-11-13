a4aa455d36f4549736b02c9d18ece4b138e3bde2
Armando Sánchez Medina <armandosanchezmedina@gmail.com>
2015-11-13 21:59:14 +0000

    Add constant for domain names file

--- src/main/java/net/sinoriel/testsphere/Constants.java
+++ src/main/java/net/sinoriel/testsphere/Constants.java
@@ -7,6 +7,7 @@ public class Constants {
     public static final String MALE_NAMES_PROPERTIES_FILE = "maleNames.txt";
     public static final String FEMALE_NAMES_PROPERTIES_FILE = "femaleNames.txt";
     public static final String SURNAMES_PROPERTIES_FILE = "surNames.txt";
+    public static final String DOMAINS_PROPERTIES_FILE = "domains.txt";
 
     public static final Double DEFAULT_PROBABILITY_FOR_MIDDLE_NAMES = 0.05D;
     public static final Double DEFAULT_PROBABILITY_FOR_DOUBLE_SURNAMES = 0.05D;
@@ -15,7 +16,6 @@ public class Constants {
 
     public static final Integer DEFAULT_AGE_ADULT_MAX = 65;
     public static final Integer DEFAULT_AGE_ADULT_MIN = 18;
-
 
 
 }
